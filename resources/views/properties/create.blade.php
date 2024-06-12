e2febd4dc97f01f6a598139337bc1bdfbe97ec7d
ArcturusSunrae <cb010914@students.apiit.lk>
2024-06-12 17:44:04 +0530

    final commit

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -37,7 +37,7 @@
                     <div class="flex items-center justify-between">
                         <button type="submit"
                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                            Update
+                            Create
                         </button>
                         <a href="{{ route('properties.index') }}"
                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
